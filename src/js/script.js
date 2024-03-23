c31b2fd2573cda593e57309a7407d0b3aee208e4
BFI01 <bfi01messages@gmail.com>
2024-03-23 20:27:35 +0000

    Add custom markers for colour control

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,63 @@
+function createColoredMarkerIcon(color) {
+    const pin = `
+        position: absolute;
+        border-radius: 50%;
+        border: 8px solid ${color};
+        width: 8px;
+        height: 8px;
+    `;
+
+    const pinArrow = `
+        position: absolute;
+        width: 0;
+        height: 0;
+        bottom: -45px;
+        left: 2px; 
+        border: 10px solid transparent;
+        border-top: 17px solid ${color};
+    `;
+
+    const markerHtml = `
+        <div style="position: relative;">
+            <div style="${pin}"></div>
+            <div style="${pinArrow}"></div>
+        </div>`;
+
+    return L.divIcon({
+        className: "custom-pin",
+        iconAnchor: [12, 37],
+        popupAnchor: [1, -28],
+        html: markerHtml
+    });
+}
+
+function addMarker(marker) {
+    const titleHTML = marker.title ? `<h1 style="text-align: center; font-weight: bold;">${marker.title}</h1>` : '';
+    const textHTML = marker.text ? `<div>${marker.text}</div>` : '';
+
+    const popupContent = `${titleHTML}${textHTML}`;
+
+    const customMarker = L.marker([marker.lat, marker.lng], {
+        icon: createColoredMarkerIcon(marker.color || DEFAULT_COLOR),
+        title: marker.title || '',
+        alt: marker.title || 'Pin'
+    }).addTo(map);
+
+    customMarker.bindPopup(popupContent);
+    customMarker.on('click', e => map.setView(e.target.getLatLng(), MAX_ZOOM));
+}
+
+// Set constants
 const MAP_WIDTH = 6684 / 2,
     MAP_HEIGHT = 4004 / 2,
     MAP_URL = 'assets/map.webp',
-    POLYGON_COLOR = '#00f',
+    DEFAULT_COLOR = '#032C98FF',
     MAX_ZOOM = 4,
     MIN_ZOOM = 2,
     INITIAL_ZOOM = 1,
     DEBUG_MODE = false; // Set to true for debugging
 
+// Load map image and create map
 const map = L.map('map', {
     attributionControl: false,
     minZoom: MIN_ZOOM,
@@ -24,29 +75,8 @@ const bounds = new L.LatLngBounds(
 L.imageOverlay(MAP_URL, bounds).addTo(map);
 map.setMaxBounds(bounds);
 
-const marker_locations = [
-    {lat: -87, lng: 375, text:'1'},
-    {lat: -95, lng: 384, text:'2'},
-    {lat: -53, lng: 314, text:'Train Station'},
-    {lat: -71, lng: 322, text:'3'},
-    {lat: -71, lng: 250, text:'4'},
-    {lat: -115, lng: 272, text:'5'},
-    {lat: -115, lng: 327, text:'6'},
-    {lat: -104, lng: 348, text:'7'},
-    {lat: -111, lng: 232, text:'8'},
-    {lat: -113, lng: 224, text:'9'},
-    {lat: -121, lng: 200, text:'10'},
-    {lat: -98, lng: 207, text:'11'},
-    {lat: -80, lng: 275, text: 'Main Building'},
-    {lat: -107, lng: 174, text:'12'},
-    {lat: -120, lng: 151, text:'13'},
-    {lat: -118, lng: 111, text:'14'},
-    {lat: -198, lng: 68, text:'15'},
-    {lat: -163, lng: 20, text:'16'},
-    {lat: -29, lng: 100, text:'Bus Stop?'},
-    {lat: -125, lng: 70, text:'17'}
-];
-const area_coords = [
+// Place area marker
+const areaCoords = [
     [-102, 191],
     [-103, 195],
     [-105, 198],
@@ -58,24 +88,24 @@ const area_coords = [
     [-111, 187],
     [-105, 187]
 ];
-
-const area = L.polygon(area_coords, {color: POLYGON_COLOR}).addTo(map);
+const area = L.polygon(areaCoords, {color: DEFAULT_COLOR}).addTo(map);
 area.bindPopup('This is a colored region.');
 area.on('click', () => map.fitBounds(area.getBounds()));
 
-marker_locations.forEach(location => addMarker(location));
+// Load and place markers from JSON file
+fetch('assets/markers.json')
+    .then(response => response.json())
+    .then(data => {
+        data.markers.forEach(marker => addMarker(marker));
+    })
+    .catch(error => console.error('Error loading markers:', error));
 
-function addMarker(location) {
-    const marker = L.marker([location.lat, location.lng]).addTo(map);
-    marker.bindPopup(location.text);
-    marker.on('click', e => map.setView(e.target.getLatLng(), MAX_ZOOM));
-}
-
+// Copy coordinates to clipboard on click
 if (DEBUG_MODE) {
     map.on('click', function(e) {
         const lat = Math.round(e.latlng.lat);
         const lng = Math.round(e.latlng.lng);
-        const formattedText = `{lat: ${lat}, lng: ${lng}, text:''},`;
+        const formattedText = `{lat: ${lat}, lng: ${lng}, title:'', text:'', color:''},`;
         navigator.clipboard.writeText(formattedText).then(() => {
             console.log('Coordinates copied to clipboard:', formattedText);
         }).catch(err => {
@@ -83,4 +113,3 @@ if (DEBUG_MODE) {
         });
     });
 }
-
